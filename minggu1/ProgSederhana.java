3b7b8ac7ac737f38eb3e776ea0ad387206660f54
Nila Sri Agustina <72902304+nilasriagustina12279@users.noreply.github.com>
2021-03-18 21:31:02 +0700

    Add files via upload

---
+++ minggu1/ProgSederhana.java
@@ -0,0 +1,23 @@
+package programSederhana ;
+
+public class ProgSederhana {
+    public static void main (string [] args){
+        soalA ();
+        soalB ();
+        soalC ();
+        soalD (); 
+    }
+    private static void soalA () {
+        hasil int = -5+8*6 ;
+        System.out.println(hasil);
+    } private static void soalB () {
+        hasil int = (55+9)%9;
+        System.out.println(hasil);
+    } private static void soalC () {
+        int hasil = 20+-3*5/8;
+        System.out.println(hasil);
+    } private static void soalD () {
+        int hasil = 5+15/3*2-8%3;
+        System.out.println(hasil);
+    }
+}
