64308f60ec1c156897d565e1231b10b35df54bf2
Nila Sri Agustina <72902304+nilasriagustina12279@users.noreply.github.com>
2021-03-18 20:23:06 +0700

    Add files via upload

---
+++ minggu2/kalkulatorsederhana.java
@@ -0,0 +1,57 @@
+package com.a11_2019_12279;
+
+/*
+Nama    : Nila Sri Agustina
+Nim     : A11.2019.12279
+ */
+public class Operasi
+{
+    public void menu()
+    {
+        System.out.println("\n====================");
+        System.out.println("Kalkulator Sederhana");
+        System.out.println("1.Penjumlahan");
+        System.out.println("2.Pengurangan");
+        System.out.println("3.Perkalian");
+        System.out.println("4.Pembagian");
+        System.out.println("5.Modulus");
+        System.out.print("Masukan Pilihan Anda = ");
+    }
+
+
+    public float operasiAritmatika(int pilihan, float nom1, float nom2)
+    {
+        float total = 0;
+        switch (pilihan)
+        {
+            case 1:
+                System.out.println(+nom1 + " + " + nom2);
+                System.out.print("Hasilnya = ");
+                total = nom1 + nom2;
+                break;
+            case 2:
+                System.out.println(+nom1 + " - " + nom2);
+                System.out.print("Hasilnya = ");
+                total = nom1 - nom2;
+                break;
+            case 3:
+                System.out.println(+nom1 + " x " + nom2);
+                System.out.print("Hasilnya = ");
+                total = nom1 * nom2;
+                break;
+            case 4:
+                System.out.println(+nom1 + " : " + nom2);
+                System.out.print("Hasilnya = ");
+                total = nom1 / nom2;
+                break;
+            case 5:
+                System.out.println(+nom1 + " % " + nom2);
+                System.out.print("Hasilnya = ");
+                total = nom1 % nom2;
+                break;
+
+        }
+        return total;
+    }
+
+}
